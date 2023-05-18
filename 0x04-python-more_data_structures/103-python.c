901bb248490366f12e292b6a43108b454c3d261c
BAISSANE <baissanee@gmail.com>
2023-05-17 19:30:55 -0700

    Updates

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,50 +1,63 @@
 #include <Python.h>
 #include <stdio.h>
 
-void print_python_list(PyObject *p) {
-    Py_ssize_t size, i;
-    PyObject *item;
+/**
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
 
-    if (!PyList_Check(p)) {
-        printf("Invalid List Object\n");
-        return;
-    }
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *item;
 
-    size = PyList_Size(p);
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	if (!PyList_Check(p))
+	{
+		printf("Invalid List Object\n");
+		return;
+	}
 
-    for (i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
+	size = PyList_Size(p);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
 
-void print_python_bytes(PyObject *p) {
-    Py_ssize_t size, i;
-    char *str;
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i;
+	char *str;
 
-    if (!PyBytes_Check(p)) {
-        printf("Invalid Bytes Object\n");
-        return;
-    }
+	if (!PyBytes_Check(p))
+	{
+		printf("Invalid Bytes Object\n");
+		return;
+	}
 
-    size = PyBytes_Size(p);
-    printf("[.] bytes object info\n");
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", PyBytes_AsString(p));
+	size = PyBytes_Size(p);
+	printf("[.] bytes object info\n");
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", PyBytes_AsString(p));
 
-    if (size > 10)
-        size = 10;
+	if (size > 10)
+		size = 10;
 
-    str = PyBytes_AsString(p);
+	str = PyBytes_AsString(p);
 
-    printf("  first %ld bytes: ", size);
-    for (i = 0; i < size; i++) {
-        printf("%02x", (unsigned char)str[i]);
-        if (i < size - 1)
-            printf(" ");
-    }
-    printf("\n");
+	printf("  first %ld bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02x", (unsigned char)str[i]);
+		if (i < size - 1)
+			printf(" ");
+	}
+	printf("\n");
 }
